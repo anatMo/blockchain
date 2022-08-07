7bc0de7f4303ff5451b7c5b323c2a69955837e00
anatMo <1.9.9.4anat@gmail.com>
2022-08-07 21:38:23 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const { getStockCloseNow, getStocksCloseArray, getEthInUsd } = require("./utils.js");
+const {
+  getStockCloseNow,
+  getStocksCloseArray,
+  getEthInUsd,
+} = require("./utils.js");
 const yahooStockAPI = require("yahoo-stock-api");
-const {getEthPriceNow,getEthPriceHistorical}= require('get-eth-price');
-
+const { getEthPriceNow, getEthPriceHistorical } = require("get-eth-price");
 
 //mysql
 let mysql = require("mysql2");
 let config = require("./config.js");
 let connection = mysql.createConnection(config);
-
 
 const {
   insertToMySql,
@@ -58,8 +60,6 @@ app.get("/getAllContracts", (req, res) => {
   connection.end();
 });
 
-
-
 app.post("/purchase", async (req, res) => {
   const { purchaseInfo } = req.body;
   const contract = purchaseInfo.contract;
@@ -70,14 +70,13 @@ app.post("/purchase", async (req, res) => {
   var stocksCloseNow = getStockCloseNow(stockCloseDict, symbol);
 
   var ethInUsd = await getEthInUsd();
-  var quantityApproved = parseInt(eth*ethInUsd/stocksCloseNow);
+  var quantityApproved = parseInt((eth * ethInUsd) / stocksCloseNow);
   var approvePurchase = quantityApproved >= 1;
-
 
   if (approvePurchase) {
     //check if a row with contract-symbol exists in the db
     let sql = `SELECT 1 FROM contracts_stocks WHERE contract= ? AND symbol=? AND purchase_rate=?`;
-    let data = [contract, symbol,stocksCloseNow];
+    let data = [contract, symbol, stocksCloseNow];
 
     connection.query(sql, data, (error, results, fields) => {
       if (error) {
@@ -96,13 +95,10 @@ app.post("/purchase", async (req, res) => {
   } else {
     res.send("not enough money to purchase even 1 stock.");
   }
-
 });
 
-
-app.post("/sell", async(req, res) => {
+app.post("/sell", async (req, res) => {
   const { sellInfo } = req.body;
-
 
   const contract = sellInfo.contract;
   const symbol = sellInfo.symbol;
@@ -124,17 +120,18 @@ app.post("/sell", async(req, res) => {
     if (error) {
       return console.error(error.message);
     }
-    if(results == 0){
+    if (results == 0) {
       res.send("No such contract");
-    }
-    var currentQuantity = results[0].quantity;
-    var updatedQuantity = currentQuantity - quantity;
-    if (updatedQuantity <= 0) {
-      deleteContractfromMySql(contract, symbol);
-      res.send({"ETH": totalETH});
-    }else{
-      updateQuantityMySql(-quantity,contract,symbol);
-      res.send({"ETH": totalETH});
+    } else {
+      var currentQuantity = results[0].quantity;
+      var updatedQuantity = currentQuantity - quantity;
+      if (updatedQuantity <= 0) {
+        deleteContractfromMySql(contract, symbol);
+        res.send({ ETH: totalETH });
+      } else {
+        updateQuantityMySql(-quantity, contract, symbol);
+        res.send({ ETH: totalETH });
+      }
     }
   });
 
@@ -147,21 +144,23 @@ app.post("/getContractsInvestedStocks", (req, res) => {
 
   let sql = `SELECT * FROM contracts_stocks where contract = ?`;
 
-  connection.query(sql,contract, async(error, results, fields) => {
+  connection.query(sql, contract, async (error, results, fields) => {
     if (error) {
       return console.error(error.message);
     }
     for (let i = 0; i < results.length; i++) {
       var stockCloseDict = await getStocksCloseArray([results[i]["symbol"]]);
-      var stocksCloseNow = getStockCloseNow(stockCloseDict, results[i]["symbol"]);
+      var stocksCloseNow = getStockCloseNow(
+        stockCloseDict,
+        results[i]["symbol"]
+      );
       results[i]["currentRate"] = stocksCloseNow;
     }
-    
+
     res.send(results);
   });
   // connection.end();
 });
-
 
 app.post("/getContractsValue", (req, res) => {
   const { info } = req.body;
@@ -171,20 +170,23 @@ app.post("/getContractsValue", (req, res) => {
   FROM usersdb.contracts_stocks 
   where contract = ?`;
 
-  connection.query(sql,contract, async(error, results, fields) => {
+  connection.query(sql, contract, async (error, results, fields) => {
     if (error) {
       return console.error(error.message);
     }
     var totalContractValueUSD = 0;
     for (let i = 0; i < results.length; i++) {
       var stockCloseDict = await getStocksCloseArray([results[i]["symbol"]]);
-      var stocksCloseNow = getStockCloseNow(stockCloseDict, results[i]["symbol"]);
-      var totalSymbolValue = (stocksCloseNow * (results[i]["quantity"]));
+      var stocksCloseNow = getStockCloseNow(
+        stockCloseDict,
+        results[i]["symbol"]
+      );
+      var totalSymbolValue = stocksCloseNow * results[i]["quantity"];
       totalContractValueUSD = totalContractValueUSD + totalSymbolValue;
     }
     var ethInUsd = await getEthInUsd();
     var totalContractValueETH = totalContractValueUSD / ethInUsd;
-    res.send({"totalContractValueETH":totalContractValueETH});
+    res.send({ totalContractValueETH: totalContractValueETH });
   });
   // connection.end();
 });
@@ -197,13 +199,10 @@ app.post("/deleteContractFromDB", (req, res) => {
   let sql = `DELETE FROM contracts_stocks
   WHERE contract=?`;
 
-  connection.query(sql,contract, (error, results, fields) => {
+  connection.query(sql, contract, (error, results, fields) => {
     if (error) {
       return console.error(error.message);
     }
     res.send("Deleted Successfully");
-    
   });
-
-
 });
